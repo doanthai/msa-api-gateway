0ff63aa4a0f28c82a9657bf702eea55378a26658
thaidq <thaidq@vtvlive.vn>
2021-12-01 21:43:41 +0700

    format code

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 dotenv.config();
 
-let services = JSON.parse(process.env['TARGET']);
+const services = JSON.parse(process.env['TARGET']);
 const port = +process.env.PORT;
 
 const app = express();
 
 if (!Array.isArray(services) || !services.length) process.exit(1);
 
-services.forEach(w => {
+services.forEach((w) => {
   w.changeOrigin = true;
   app.use(createProxyMiddleware(w));
-})
+});
 
 app.listen(port);
 
